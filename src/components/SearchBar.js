adf575291d64f5934a1c9018374c89b2df348685
zoristic <sokyyy@gmail.com>
2022-10-23 06:09:47 +0200

    Food app

---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name='search' style={styles.iconStyle} />
+      <TextInput
+        autoCapitalize='none'
+        autoCorrect={false}
+        style={styles.inputStle}
+        placeholder='Search'
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 15,
+    backgroundColor: '#DBD9D9',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row',
+  },
+  inputStle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15,
+  },
+});
+
+export default SearchBar;
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
+
+const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
+
+  return (
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage} </Text> : null}
+      <Text>We have found {results.length} results</Text>
+      <ResultsList title='Cost effective' />
+      <ResultsList title='Bit pricier' />
+      <ResultsList title='Big Spender' />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
