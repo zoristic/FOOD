ad82607d124b938c22b26147215fc346c000fdba
zoristic <sokyyy@gmail.com>
2022-10-24 12:23:06 +0200

    API remove

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    Authorization:
-      'Bearer THLJQt6hNixuQLx8rd4IOHfjVRpAFBeN1wLrudcGQXBcB0MTrU2GvN77T-FWnoXhawfpaJNhzg9ktA06O13oXRHuZ7ylDjqgjujZ00p9tdfnt9dvF9laqc4gvwhTY3Yx',
+    Authorization: 'Bearer Enter you api key',
   },
 });
